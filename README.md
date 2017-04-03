ecf6753d19a391411076a146a220ff619ae66f07
Alex Shortt <alexander.shortt@gmail.com>
2017-04-03 21:32:54 +0000

    havent pushed in a while so here

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # terra-labs-website
 Website for Terra Labs
+
+//toodooo
+-scale text for phone
+-fix the non-perfect scroll
--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,77 @@
 function getWindowScroll(num) {
     var s = $(window).scrollTop(),
         d = $(".section:nth-child(" + num + ")").offset().top;
-        c = $(window).height();
+    c = $(window).height();
 
-    return Math.abs(( d - s) / (d + c));
+    return Math.abs((d - s) / (d + c));
 }
 
+var maxBlur = 8;
+var blurOffset = 0;
+
 $(window).scroll(function(event) {
-    $('.section:nth-child(1) .background-image').css('webkitFilter', "blur(" + (Math.round(10 * getWindowScroll(1) * 100) / 100) + "px)");
-    $('.section:nth-child(2) .background-image').css('webkitFilter', "blur(" + (1 + Math.round(10 * getWindowScroll(2) * 100) / 100) + "px)");
+    if (getWindowScroll(1) < 0.7) {
+        $('.section:nth-child(1) .background-image').css('webkitFilter', "blur(" + (blurOffset + (Math.round(maxBlur * getWindowScroll(1) * 10) / 10)) + "px)");
+    }
+    if (getWindowScroll(2) < 0.7) {
+        $('.section:nth-child(2) .background-image').css('webkitFilter', "blur(" + (blurOffset + (Math.round(maxBlur * getWindowScroll(2) * 10) / 10)) + "px)");
+    }
+    if (getWindowScroll(4) < 0.7) {
+        $('.section:nth-child(4) .background-image').css('webkitFilter', "blur(" + (2 + blurOffset + (Math.round(maxBlur * getWindowScroll(4) * 10) / 10)) + "px)");
+    }
+
 });
+
+function fixVHChrome() {
+    var options = [{
+        selector: '.section',
+        property: 'min-height',
+        vh: 110,
+    }, {
+        selector: '.section:nth-child(2)',
+        property: 'top',
+        vh: 110
+    }, {
+        selector: '.section:nth-child(3)',
+        property: 'top',
+        vh: 220
+    }, {
+        selector: '.section:nth-child(4)',
+        property: 'top',
+        vh: 330
+    }];
+
+    var vhFix = new VHChromeFix(options);
+}
+
+function validateEmail(email) {
+    var ex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return ex.test(email);
+}
+
+
+function initSignup() {
+    var email = document.getElementById('email'),
+        button = document.getElementById('button'),
+        label = document.getElementById('email-label');
+
+    email.addEventListener('keydown', function() {
+        var email = this.value;
+
+        if (validateEmail(email)) {
+            button.classList.add('is-active');
+        }
+    });
+
+    button.addEventListener('click', function(e) {
+        e.preventDefault();
+        this.classList.add('is-done', 'is-active');
+        label.innerHTML = "";
+        email.value = "";
+
+        setTimeout(function() {
+            button.innerHTML = "Thanks!"
+        }, 500);
+    });
+
+}
